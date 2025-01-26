0d185f5f7759cb6ada09759ec23e06b0b510025a
WikiCoding <tiagomscastro@gmail.com>
2025-01-26 22:18:08 +0000

    Added AsParallel example

---
+++ IEnumerable_AsParallel/AsParallelConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine($"Running on {Thread.CurrentThread.ManagedThreadId}");
+
+var stopWatch = Stopwatch.StartNew();
+var enumerable = list.AsParallel().Where(i => i % 2 == 0).Select(i =>
+{
+  Console.WriteLine($"Squaring {i} on thread {Thread.CurrentThread.ManagedThreadId}");
+  return i * i;
+}).ToList();
+stopWatch.Stop();
+
+Console.WriteLine($"Running on back at {Thread.CurrentThread.ManagedThreadId}");
+
+Console.WriteLine($"Time taken: {stopWatch.ElapsedMilliseconds}ms");
